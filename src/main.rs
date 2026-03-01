e59ee95403ee51a7777b448cf12bd1fba5e62ea8
Kilari Teja <kilariteja9@gmail.com>
2026-03-02 02:23:42 +0530

    x: Draw to basic window

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,129 @@
 use smithay_client_toolkit::{
     compositor::{CompositorHandler, CompositorState},
-    delegate_compositor, delegate_output, delegate_registry,
+    delegate_compositor, delegate_output, delegate_registry, delegate_shm, delegate_xdg_shell,
+    delegate_xdg_window,
     output::{OutputHandler, OutputState},
     registry::{ProvidesRegistryState, RegistryState},
     registry_handlers,
+    shell::{
+        WaylandSurface,
+        xdg::{
+            self, XdgShell, XdgSurface,
+            window::{Window, WindowDecorations},
+        },
+    },
+    shm::{
+        Shm, ShmHandler,
+        slot::{Buffer, SlotPool},
+    },
 };
-use wayland_client::{Connection, globals::registry_queue_init};
+use wayland_client::{Connection, Proxy, globals::registry_queue_init, protocol::wl_shm};
 
 fn main() {
     let conn = Connection::connect_to_env().expect("wl connection");
 
-    let (globals, event_queue) = registry_queue_init::<State>(&conn).expect("wl queue init");
+    let (globals, mut event_queue) = registry_queue_init::<State>(&conn).expect("wl queue init");
     let q_handle = event_queue.handle();
 
-    let registry_state = RegistryState::new(&globals);
     let output_state = OutputState::new(&globals, &q_handle);
-    let _compositor_state =
-        CompositorState::bind(&globals, &q_handle).expect("wl compositor state");
+    let registry_state = RegistryState::new(&globals);
 
-    let _state = State {
+    let compositor_state = CompositorState::bind(&globals, &q_handle).expect("wl compositor state");
+    let xdg_shell = XdgShell::bind(&globals, &q_handle).expect("xdgshell");
+
+    let surface = compositor_state.create_surface(&q_handle);
+    let window = xdg_shell.create_window(surface, WindowDecorations::ServerDefault, &q_handle);
+
+    window.commit();
+
+    let shm = Shm::bind(&globals, &q_handle).expect("wl_shm");
+    let pool = SlotPool::new(256 * 256, &shm).expect("wl_shm_pool");
+
+    let mut state = State {
+        count: 0,
+        color: 255,
+
         registry_state,
         output_state,
+
+        window,
+        width: 0,
+        height: 0,
+
+        shm,
+        pool,
+        buffer: None,
     };
+    loop {
+        event_queue
+            .blocking_dispatch(&mut state)
+            .expect("event_dispatch");
+    }
 }
 
 pub struct State {
+    count: u64,
+    color: u8,
+
     registry_state: RegistryState,
     output_state: OutputState,
+
+    window: Window,
+    width: u16,
+    height: u16,
+
+    shm: Shm,
+    pool: SlotPool,
+    buffer: Option<Buffer>,
+}
+
+impl State {
+    fn make_buffer(&mut self) {
+        let (buffer, _) = self
+            .pool
+            .create_buffer(
+                self.width.into(),
+                self.height.into(),
+                (self.width * 4).into(),
+                wl_shm::Format::Argb8888,
+            )
+            .expect("wl_shm_pool");
+
+        let surface = self.window.wl_surface();
+        buffer.attach_to(surface).expect("buffer");
+        surface.commit();
+
+        self.buffer = Some(buffer);
+    }
+
+    fn draw(&mut self, qh: &wayland_client::QueueHandle<Self>) {
+        self.color = self.color.wrapping_add(1);
+
+        let (buffer, canvas) = self
+            .pool
+            .create_buffer(
+                self.width.into(),
+                self.height.into(),
+                (self.width * 4).into(),
+                wl_shm::Format::Argb8888,
+            )
+            .expect("create buffer");
+
+        for (no, pixel) in canvas.chunks_exact_mut(4).enumerate() {
+            if no % 2 == 0 {
+                pixel[0] = 255;
+                pixel[1] = self.color;
+                pixel[2] = self.color;
+                pixel[3] = self.color;
+            }
+        }
+
+        let surface = self.window.wl_surface();
+        buffer.attach_to(surface).expect("attach");
+        surface.damage_buffer(0, 0, self.width.into(), self.height.into());
+        surface.frame(qh, surface.clone());
+        surface.commit();
+    }
 }
 
 // Required for compositor delegation.
@@ -39,9 +136,9 @@ impl OutputHandler for State {
         &mut self,
         _conn: &Connection,
         _qh: &wayland_client::QueueHandle<Self>,
-        _output: wayland_client::protocol::wl_output::WlOutput,
+        output: wayland_client::protocol::wl_output::WlOutput,
     ) {
-        println!("wl_output: new_output");
+        println!("wl_output: new_output ({:?})", output.id());
     }
 
     fn update_output(
@@ -88,11 +185,13 @@ impl CompositorHandler for State {
     fn frame(
         &mut self,
         _conn: &Connection,
-        _qh: &wayland_client::QueueHandle<Self>,
+        qh: &wayland_client::QueueHandle<Self>,
         _surface: &wayland_client::protocol::wl_surface::WlSurface,
         _time: u32,
     ) {
-        println!("wl_compositor: frame");
+        self.count += 1;
+        println!("wl_compositor: frame ({})", self.count);
+        self.draw(qh);
     }
 
     fn surface_enter(
@@ -116,6 +215,45 @@ impl CompositorHandler for State {
     }
 }
 delegate_compositor!(State);
+
+impl xdg::window::WindowHandler for State {
+    fn request_close(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _window: &xdg::window::Window,
+    ) {
+        println!("xdgshell: request_close");
+    }
+
+    fn configure(
+        &mut self,
+        _conn: &Connection,
+        qh: &wayland_client::QueueHandle<Self>,
+        window: &xdg::window::Window,
+        _configure: xdg::window::WindowConfigure,
+        _serial: u32,
+    ) {
+        println!("xdgshell: configure");
+
+        window.set_window_geometry(0, 0, 256, 256);
+        window.set_app_id("xyz.ksdme.wl");
+        self.width = 256;
+        self.height = 256;
+
+        self.draw(qh);
+    }
+}
+delegate_xdg_shell!(State);
+delegate_xdg_window!(State);
+
+impl ShmHandler for State {
+    fn shm_state(&mut self) -> &mut Shm {
+        println!("wl_shm: shm_state");
+        &mut self.shm
+    }
+}
+delegate_shm!(State);
 
 // Required to start the queue and keep the globals up to date.
 impl ProvidesRegistryState for State {

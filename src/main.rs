bf039ad64aa4e71e505636c6ef8e9673a5668d90
Kilari Teja <kilariteja9@gmail.com>
2026-03-01 19:52:14 +0530

    Add basic compositor, output dispatcher

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,128 @@
+use smithay_client_toolkit::{
+    compositor::{CompositorHandler, CompositorState},
+    delegate_compositor, delegate_output, delegate_registry,
+    output::{OutputHandler, OutputState},
+    registry::{ProvidesRegistryState, RegistryState},
+    registry_handlers,
+};
+use wayland_client::{Connection, globals::registry_queue_init};
+
 fn main() {
-    println!("Hello, world!");
+    let conn = Connection::connect_to_env().expect("wl connection");
+
+    let (globals, event_queue) = registry_queue_init::<State>(&conn).expect("wl queue init");
+    let q_handle = event_queue.handle();
+
+    let registry_state = RegistryState::new(&globals);
+    let output_state = OutputState::new(&globals, &q_handle);
+    let _compositor_state =
+        CompositorState::bind(&globals, &q_handle).expect("wl compositor state");
+
+    let _state = State {
+        registry_state,
+        output_state,
+    };
 }
+
+pub struct State {
+    registry_state: RegistryState,
+    output_state: OutputState,
+}
+
+// Required for compositor delegation.
+impl OutputHandler for State {
+    fn output_state(&mut self) -> &mut smithay_client_toolkit::output::OutputState {
+        &mut self.output_state
+    }
+
+    fn new_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _output: wayland_client::protocol::wl_output::WlOutput,
+    ) {
+        println!("wl_output: new_output");
+    }
+
+    fn update_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _output: wayland_client::protocol::wl_output::WlOutput,
+    ) {
+        println!("wl_output: update_output");
+    }
+
+    fn output_destroyed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _output: wayland_client::protocol::wl_output::WlOutput,
+    ) {
+        println!("wl_output: output_destroyed");
+    }
+}
+delegate_output!(State);
+
+impl CompositorHandler for State {
+    fn scale_factor_changed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _surface: &wayland_client::protocol::wl_surface::WlSurface,
+        _new_factor: i32,
+    ) {
+        println!("wl_compositor: scale_factor_changed");
+    }
+
+    fn transform_changed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _surface: &wayland_client::protocol::wl_surface::WlSurface,
+        _new_transform: wayland_client::protocol::wl_output::Transform,
+    ) {
+        println!("wl_compositor: transform_changed");
+    }
+
+    fn frame(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _surface: &wayland_client::protocol::wl_surface::WlSurface,
+        _time: u32,
+    ) {
+        println!("wl_compositor: frame");
+    }
+
+    fn surface_enter(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _surface: &wayland_client::protocol::wl_surface::WlSurface,
+        _output: &wayland_client::protocol::wl_output::WlOutput,
+    ) {
+        println!("wl_compositor: surface_enter");
+    }
+
+    fn surface_leave(
+        &mut self,
+        _conn: &Connection,
+        _qh: &wayland_client::QueueHandle<Self>,
+        _surface: &wayland_client::protocol::wl_surface::WlSurface,
+        _output: &wayland_client::protocol::wl_output::WlOutput,
+    ) {
+        println!("wl_compositor: surface_leave");
+    }
+}
+delegate_compositor!(State);
+
+// Required to start the queue and keep the globals up to date.
+impl ProvidesRegistryState for State {
+    fn registry(&mut self) -> &mut RegistryState {
+        &mut self.registry_state
+    }
+
+    registry_handlers!();
+}
+delegate_registry!(State);
